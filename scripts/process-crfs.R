22d0108d16e9fa3368ba6c4bb4c1bf02e737b73f
Oscar de León <odeleongt@gmail.com>
2018-10-01 17:39:33 -0600

    Script to process CRFs pdf

---
+++ scripts/process-crfs.R
@@ -0,0 +1,19 @@
+#------------------------------------------------------------------------------*
+# Process CRFs pdf pack from the Emory RedCap project
+#------------------------------------------------------------------------------*
+
+
+#------------------------------------------------------------------------------*
+# Prepare script environment ----
+#------------------------------------------------------------------------------*
+# See https://github.com/ropensci/pdftools for special requirements
+#------------------------------------------------------------------------------*
+
+# Load used packages
+library(package = "tidyverse") # work with dataframes
+library(package = "pdftools")  # read pdf contents using the poppler library
+
+
+
+
+# End of script
